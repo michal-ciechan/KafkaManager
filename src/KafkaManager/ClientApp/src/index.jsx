4947f19c949b7df4846ad3411c5a20546e41d458
Michal Ciechan <lndcobra@hotmail.com>
2021-04-14 15:11:46 +0100

    Initial Version with Snowpack

---
+++ src/KafkaManager/ClientApp/src/index.jsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+console.log("Running :D");
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>,
+  rootElement);
+
+//
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
